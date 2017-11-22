27a8248171085d0460a907322dabbe44b71f6b7b
Balachandar <29444683+Balachandarsmr@users.noreply.github.com>
2017-11-22 09:42:06 +0530

    Add files via upload

--- app/src/main/java/com/pokemonshowdown/app/BattleFragment.java
+++ app/src/main/java/com/pokemonshowdown/app/BattleFragment.java
@@ -2127,7 +2127,7 @@ public class BattleFragment extends Fragment {
                 } else if (zMovecheckBox.isChecked()) {
                     command = "move " + (moveId + 1) + " zmove";
                 } else if (UltraBurstcheckBox.isChecked()) {
-                    command = "move " + (moveId + 1);
+                    command = "move " + (moveId + 1) + " ultraburst";
                 } else {
                     command = "move " + (moveId + 1);
                 }
